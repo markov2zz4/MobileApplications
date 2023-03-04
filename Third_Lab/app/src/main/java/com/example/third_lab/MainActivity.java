7640765dacb7b79dc39773ac04af4f2e5e286bf0
SHpetAI <shpetai@DOM201.LOCAL>
2023-03-04 12:19:54 +0700

    Added Third lab with create new activity

---
+++ Third_Lab/app/src/main/java/com/example/third_lab/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.third_lab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button press_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OnClick(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+//https://imgur.com/a/DBnBX3Z
+
+
+
+}
