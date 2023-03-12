defa4db420ab7afc12e18ac4332c128fcc8dc036
MarkoW <albertnov@internet.ru>
2023-03-13 02:57:48 +0700

    Added third lab successfully

--- Third_Lab/app/src/main/java/com/example/third_lab/SecondActivity.java
+++ Third_Lab/app/src/main/java/com/example/third_lab/SecondActivity.java
@@ -3,12 +3,28 @@ package com.example.third_lab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private TextView NumberLab;
+    private TextView Student;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        NumberLab = findViewById(R.id.numberoflesson);
+        NumberLab.setTextSize(20);
+        NumberLab.setPadding(16, 16, 16, 16);
+        NumberLab.setText("Лабораторная работа № 3");
+
+        Student = findViewById(R.id.student);
+        Student.setTextSize(20);
+        Student.setText("Работу выполнил Шпет Альберт 007г2");
+
     }
 }
